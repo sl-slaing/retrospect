80e0a5248996561c9a4ca69c9606be8f4aebcc41
Simon Laing <slaing@scottlogic.com>
2019-12-10 14:24:47 +0000

    Interact with cookies correctly

--- retrospect.core/src/main/java/com/example/retrospect/core/models/HttpRequestWrapper.java
+++ retrospect.core/src/main/java/com/example/retrospect/core/models/HttpRequestWrapper.java
@@ -4,12 +4,29 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
 @Service
 @Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class HttpRequestWrapper {
+    private final HttpServletRequest request;
+
+    public HttpRequestWrapper(HttpServletRequest request) {
+        this.request = request;
+    }
 
     public String getCookie(String cookieName) {
-        return null;
+        if (request.getCookies() == null){
+            return null;
+        }
+
+        return Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals(cookieName))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
     }
 }
 
--- retrospect.core/src/main/java/com/example/retrospect/core/models/HttpResponseWrapper.java
+++ retrospect.core/src/main/java/com/example/retrospect/core/models/HttpResponseWrapper.java
@@ -4,14 +4,33 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Service
 @Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class HttpResponseWrapper {
 
+    private final HttpServletResponse response;
+
+    public HttpResponseWrapper(HttpServletResponse response) {
+        this.response = response;
+    }
+
     public void setSessionCookie(String cookieName, String value) {
+        var cookie = new Cookie(cookieName, value);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
     }
 
     public void removeSessionCookie(String cookieName) {
+        var cookie = new Cookie(cookieName, null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
 
+        response.addCookie(cookie);
     }
 }
